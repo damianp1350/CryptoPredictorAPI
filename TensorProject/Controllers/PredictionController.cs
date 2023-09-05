a162dda136f3e633945b11346fe9ed4e13d9309a
damia <damia@LAPTOP-47DNJPEH>
2023-09-05 12:57:54 +0200

    Added controller

--- TensorProject/Controllers/PredictionController.cs
+++ TensorProject/Controllers/PredictionController.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TensorProject.Models;
+using TensorProject.Services;
 
 [Route("api/[controller]")]
 [ApiController]
 public class PredictionController : ControllerBase
 {
-    private readonly BinanceDbContext _context;
+    private readonly TensorFlowModelService _tensorFlowModelService;
 
-    public PredictionController(BinanceDbContext dbContext)
+    public PredictionController()
     {
-        _context = dbContext;
+        _tensorFlowModelService = new TensorFlowModelService();
+    }
+
+    [HttpPost("predict")]
+    public IActionResult Predict([FromBody] BitcoinPriceInput input)
+    {
+        if (input == null || input.CloseTime == 0)
+        {
+            return BadRequest("Invalid input data.");
+        }
+
+        var predictedPriceOutput = _tensorFlowModelService.Predict(input);
+
+        return Ok(predictedPriceOutput);
     }
 }
